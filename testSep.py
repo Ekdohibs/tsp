f0b7c6a42e8eea0e5a12d05761644a21d35a20d0
daSau <david.sokipik@wanadoo.fr>
2016-12-27 17:41:30 +0100

    tests de l'algo de separation

--- testSep.py
+++ testSep.py
@@ -1,12 +1,12 @@
 from parser import parse_file
 from separation import separation
 
-name = "burma14.tsp" #3001
+#name = "burma14.tsp" #3001
 #name = "gr17.tsp" #1684
 #name = "gr21.tsp" #2707
-#name = "eil51.tsp" #416.5
+#name = "eil51.tsp" #416.5, 7s
 #name = "gr24.tsp" #1224.5
-#name = "st70.tsp" #623.5
+name = "st70.tsp" #623.5, 31s
 #name = "gr48.tsp" #4769
 #name = "pr76.tsp" #98994.5
 
