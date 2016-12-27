e7c620773d4245062cc6e19b5b755ef872d326ae
daSau <david.sokipik@wanadoo.fr>
2016-12-27 17:37:22 +0100

    algo de separation ok !

--- separation.py
+++ separation.py
@@ -1,10 +1,7 @@
 import networkx as nx
-import scipy as sc
+from scipy.optimize import linprog
 import numpy as np
-
-
-def numEdge(d, a):
-    return min(d, a)*nbN + max(d, a)
+from min_cut import stoer_wagner_nx
 
 def initGraph(Adj):
     G = nx.Graph()
@@ -18,55 +15,68 @@ def initObj(G):
     c = []
     extToEdge = {}
     for e in G.edges():
+        extToEdge[ (e[0], e[1]) ] = len(c)
+        extToEdge[ (e[1], e[0]) ] = len(c)
         c.append(G[ e[0] ][ e[1] ]['weight'])
-        extToEdge[ e[0] ][ e[1] ] = len(c) - 1
-        extToEdge[ e[1] ][ e[0] ] = len(c) - 1
 
     return c, extToEdge
+
+def initCstr(G, extToEdge):
+    nbN, nbE = len(G.nodes()), len(G.edges())
+    b_eq = 2*np.ones((nbN, 1))
+    A_eq = np.zeros((nbN, nbE))
+
+    for nd in G.nodes():
+        for nei in G[nd]:
+            A_eq[nd][ extToEdge[(nd, nei)] ] = 1
+
+    return A_eq, b_eq
 
 def subG(G, x, extToEdge):
     nbN = len(G.nodes())
     subG = nx.Graph()
     for (d, a) in G.edges():
-        subG.add_edge(d, a, weight = G[d][a]['weight'] * x[ extToEdge[d][a] ])
+        if(x[ extToEdge[(d, a)] ] != 0):
+            subG.add_edge(d, a, weight = G[d][a]['weight'] * x[ extToEdge[(d, a)] ])
     
     return subG
 
-def addSubTour(A_ub, b_ub, G, part, extToEdge):
+def addSubTour(A_ub, b_ub, G, cut, extToEdge):
     nbN = len(G.nodes())
     
     newCstr = [0]*len(G.edges())
+     
+    for d in cut[0]:
+        for a in cut[1]:
+            newCstr[ extToEdge[(d, a)] ] = -1
 
-    for d in part:
-        for a in range(nbN):
-            if(a <> d and (a not in part)):
-                newCstr[ extToEdge[d][a] ] = 1
-
-    return A_ub.append(newCstr), b_ub.append(2)
+    return A_ub.append(newCstr), b_ub.append(-2)
 
 def separation(Adj):
     G = initGraph(Adj)
-    nbN, nbE = len(G.nodes()), len(G.edges())
     
     c, extToEdge = initObj(G)
-    A_eq = np.ones((nbN, nbE))
-    b_eq = np.ones((nbN, 1))
+    
+    A_eq, b_eq = initCstr(G, extToEdge)
    
-    #mettre des 0 sur les arêtes (noeud, noeud)...
-
     A_ub = []
     b_ub = []
-    res = sc.optimize.linprog(c, A_eq = A_eq, b_eq = b_eq, bounds=((0, 1)))
+    res = linprog(c, A_eq = A_eq, b_eq = b_eq, bounds=((0, 1)))
     x = res.x
-
-    while(True):
+    print(x) 
+    n = 100
+    while(n < 100):
         sG = subG(G, x, extToEdge)
-        cutV, part = nx.stoer_wagner(sG)
+        cutV, cut = stoer_wagner_nx(sG)
+        print(cutV, cut)
         if(cutV >= 2):
             break
-        A_ub, b_ub = addSubTour(A_ub, b_ub, G, part[0], extToEdge)
+        A_ub, b_ub = addSubTour(A_ub, b_ub, G, cut, extToEdge)
         
-        res = sc.optimize.linprog(c, A_eq = A_eq, b_eq = b_eq, A_ub = A_ub, b_ub = b_ub, bounds=((0, 1)))
+        res = linprog(c, A_eq = A_eq, b_eq = b_eq, A_ub = A_ub, b_ub = b_ub, bounds=((0, 1)))
         x = res.x
+        
+        n += 1
+
 
     return x, res.fun
